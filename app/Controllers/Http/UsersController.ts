b78414358ac12818c2ac1f8ed3e32241453f439d
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-03-26 17:28:15 +0100

    feat: implemented update user method

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -22,4 +22,19 @@ export default class UsersController {
     const user = await User.create(userPayload)
     return response.created({ user })
   }
+
+  public async update({ request, response }: HttpContextContract) {
+    const { email, password, avatar } = request.only(['email', 'avatar', 'password'])
+    const id = request.param('id')
+
+    const user = await User.findOrFail(id)
+
+    user.email = email
+    user.password = password
+    if (avatar) user.avatar = avatar
+
+    await user.save()
+
+    return response.ok({ user })
+  }
 }
