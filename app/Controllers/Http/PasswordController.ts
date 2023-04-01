2d026c9f765efac8336ab27d2748320e10b636ad
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-01 12:27:01 +0100

    refactr (PasswordController): implemented creation token logic to save

--- app/Controllers/Http/PasswordController.ts
+++ app/Controllers/Http/PasswordController.ts
@@ -1,12 +1,22 @@
 import Mail from '@ioc:Adonis/Addons/Mail'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
+import { randomBytes } from 'crypto'
+import { promisify } from 'util'
 
 export default class PasswordController {
   public async forgotPassword({ request, response }: HttpContextContract) {
     const { email, resetPasswordUrl } = request.only(['email', 'resetPasswordUrl'])
 
     const user = await User.findByOrFail('email', email)
+
+    const random = await promisify(randomBytes)(24)
+
+    const token = random.toString('hex')
+
+    await user.related('tokens').updateOrCreate({ userId: user.id }, { token })
+
+    const resetPasswordUrlWithToken = `${resetPasswordUrl}?token=${token}`
 
     await Mail.send((message) => {
       message
@@ -16,7 +26,7 @@ export default class PasswordController {
         .htmlView('email/forgotpassword', {
           productName: 'Roleplay',
           name: user.username,
-          resetPasswordUrl: resetPasswordUrl,
+          resetPasswordUrl: resetPasswordUrlWithToken,
         })
     })
     return response.noContent()
