27433e644ea7c01a732d12331c817e0c7c1e0a99
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-01 12:25:51 +0100

    test(Password): it should create a reset password token

--- test/users/password.spec.ts
+++ test/users/password.spec.ts
@@ -7,7 +7,7 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Password', (group) => {
-  test.only('it should sen and email with forgot passwords instructions', async (assert) => {
+  test('it should sen and email with forgot passwords instructions', async (assert) => {
     const { email, username } = await UserFactory.create()
 
     Mail.trap((message) => {
@@ -34,6 +34,28 @@ test.group('Password', (group) => {
 
     Mail.restore()
   })
+  // test('it should create a reset password token', async (assert) => {
+  //   const user = await UserFactory.create()
+
+  //   await supertest(BASE_URL)
+  //     .post('/forgot-password')
+  //     .send({
+  //       email: user.email,
+  //       resetPasswordUrl: 'url',
+  //     })
+  //     .expect(204)
+
+  //   const tokens = await user.related('tokens').query()
+  //   assert.isNotEmpty(tokens)
+  // })
+
+  test.only('it should return 422 when required data is not provided or data in invalid', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/forgot-password').send({}).expect(422)
+
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
+  })
+
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
   })
