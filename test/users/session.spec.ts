b7028c0d36e14952ccf913aeed62ba6b963cb6e3
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-02 23:12:51 +0100

    test(Session): should revoke token 200  when user signs out

--- test/users/session.spec.ts
+++ test/users/session.spec.ts
@@ -39,7 +39,7 @@ test.group('Session', async (group) => {
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 400)
   })
-  test.only('it should return 400 when credencials are invalid', async (assert) => {
+  test('it should return 400 when credencials are invalid', async (assert) => {
     const { email } = await UserFactory.create()
     const { body } = await supertest(BASE_URL)
       .post('/sessions')
@@ -53,6 +53,47 @@ test.group('Session', async (group) => {
     assert.equal(body.status, 400)
     assert.equal(body.message, 'invalid credentials')
   })
+  test('it should return 200 user signs out', async () => {
+    const plainPassword = 'test'
+
+    const { email } = await UserFactory.merge({ password: plainPassword }).create()
+
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email, password: plainPassword })
+      .expect(201)
+
+    const apiToken = body.token
+
+    await supertest(BASE_URL)
+      .delete('/sessions')
+      .set('Authorization', `Bearer ${apiToken.token}`)
+      .expect(200)
+  })
+
+  test.only('it should revoke token 200  when user signs out', async (assert) => {
+    const plainPassword = 'test'
+
+    const { email } = await UserFactory.merge({ password: plainPassword }).create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email, password: plainPassword })
+      .expect(201)
+
+    const apiToken = body.token
+
+    await Database.query().select('*').from('api_tokens')
+
+    await supertest(BASE_URL)
+      .delete('/sessions')
+      .set('Authorization', `Bearer ${apiToken.token}`)
+      .expect(200)
+
+    const token = await Database.query().select('*').from('api_tokens')
+
+    assert.isEmpty(token)
+  })
+
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
   })
