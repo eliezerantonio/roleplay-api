727d448b00c27b51cb56e86670b3f12f7cdbaf56
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-02 21:06:26 +0100

    feat: implemented  sessions worked in routes, controller e tests and configure type token

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -4,7 +4,10 @@ export default class SessionsController {
   public async store({ request, response, auth }: HttpContextContract) {
     const { email, password } = request.only(['email', 'password'])
 
-    await auth.attempt(email, password)
-    return response.created({ user: auth.user })
+    const token = await auth.use('api').attempt(email, password, {
+      expiresIn: '2hours',
+    })
+
+    return response.created({ user: auth.user, token })
   }
 }
