69d593070cf85632584cdb03a0596ef57f7da1cc
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-02 21:46:38 +0100

    test(Session): should return 400 when credencials are invalid

--- test/users/session.spec.ts
+++ test/users/session.spec.ts
@@ -33,11 +33,25 @@ test.group('Session', async (group) => {
     assert.isDefined(body.token, 'token Undefined')
     assert.equal(body.user.id, id)
   })
-  test.only('it should return 400 when credencials are not provided', async (assert) => {
+  test('it should return 400 when credencials are not provided', async (assert) => {
     const { body } = await supertest(BASE_URL).post('/sessions').send({}).expect(400)
 
     assert.equal(body.code, 'BAD_REQUEST')
     assert.equal(body.status, 400)
+  })
+  test.only('it should return 400 when credencials are invalid', async (assert) => {
+    const { email } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({
+        email,
+        password: 'test',
+      })
+      .expect(400)
+
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 400)
+    assert.equal(body.message, 'invalid credentials')
   })
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
