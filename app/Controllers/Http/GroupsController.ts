c0f34137c0cedb10e70ce8e887c2f3185b668688
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-03 13:01:59 +0100

    feat(GroupController): implemented field validation

--- app/Controllers/Http/GroupsController.ts
+++ app/Controllers/Http/GroupsController.ts
@@ -1,9 +1,10 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Group from 'App/Models/Group'
+import CreateGroupValidator from 'App/Validators/CreateGroupValidator'
 
 export default class GroupsController {
   public async store({ request, response }: HttpContextContract) {
-    const groupPayload = request.all()
+    const groupPayload = await request.validate(CreateGroupValidator)
     const group = await Group.create(groupPayload)
 
     return response.created({ group })
