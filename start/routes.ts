b122c2926754ac03d3ea405731cbc77b251ba391
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-03 12:50:59 +0100

    feat(Group): creation store group route

--- start/routes.ts
+++ start/routes.ts
@@ -26,7 +26,11 @@ Route.get('/', async () => {
 
 Route.post('/users', 'UsersController.store')
 Route.put('/users/:id', 'UsersController.update').middleware('auth')
+
 Route.post('/forgot-password', 'PasswordController.forgotPassword')
 Route.post('/reset-password', 'PasswordController.resetPassword')
+
 Route.post('/sessions', 'SessionsController.store')
 Route.delete('/sessions', 'SessionsController.destroy')
+
+Route.post('/groups', 'GroupsController.store')
