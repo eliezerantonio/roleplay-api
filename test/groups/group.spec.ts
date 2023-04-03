62a4af4a50080c0fc46c0889db85f28a8da5a55f
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-03 12:49:03 +0100

    test(Group):should create a group

---
+++ test/groups/group.spec.ts
@@ -0,0 +1,38 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Group', (group) => {
+  test.only('it should create a group', async (assert) => {
+    const user = await UserFactory.create()
+
+    const groupPayload = {
+      name: 'test',
+      description: 'test',
+      schedule: 'test',
+      location: 'test',
+      chronic: 'test',
+      master: user.id,
+    }
+
+    const { body } = await supertest(BASE_URL).post('/groups').send(groupPayload).expect(201)
+    assert.exists(body.group, 'Group Undefined')
+    assert.equal(body.group.name, groupPayload.name)
+    assert.equal(body.group.description, groupPayload.description)
+    assert.equal(body.group.schedule, groupPayload.schedule)
+    assert.equal(body.group.location, groupPayload.location)
+    assert.equal(body.group.chronic, groupPayload.chronic)
+    assert.equal(body.group.master, groupPayload.master)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+})
