fe3907017979dfe3ba281fde791506beab923f15
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-04 17:04:09 +0100

    feat(GroupRequestController): verify existingGroupRequest before save

--- app/Controllers/Http/GroupRequestsController.ts
+++ app/Controllers/Http/GroupRequestsController.ts
@@ -1,10 +1,20 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequestException from 'App/Exceptions/BadRequestException'
 import GroupRequest from 'App/Models/GroupRequest'
 
 export default class GroupRequestsController {
   public async store({ request, response, auth }: HttpContextContract) {
     const groupId = request.param('groupId') as number
     const userId = auth.user!.id
+
+    const existingGroupRequest = await GroupRequest.query()
+      .where('groupId', groupId)
+      .andWhere('userId', userId)
+      .first()
+
+    if (existingGroupRequest) {
+      throw new BadRequestException('group request already exists', 409)
+    }
 
     const groupRequest = await GroupRequest.create({ groupId, userId })
     await groupRequest.refresh()
