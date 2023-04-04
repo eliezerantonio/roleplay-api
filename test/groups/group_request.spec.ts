531dc2b72c52ff38a81764497bdf100ee0f050cb
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-04 13:00:06 +0100

    test(GroupRequest): should create a group request

---
+++ test/groups/group_request.spec.ts
@@ -0,0 +1,51 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import User from 'App/Models/User'
+import { GroupFactory, UserFactory } from 'Database/factories'
+import test from 'japa'
+
+import supertest from 'supertest'
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+let token = ''
+let user = {} as User
+test.group('Group Request', (group) => {
+  test.only('it should create a group request', async (assert) => {
+    const { id } = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: id }).create()
+
+    const { body } = await await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({})
+      .expect(201)
+
+    assert.exists(body.groupRequest, 'GroupRequest undefined')
+    assert.equal(body.groupRequest.userId, user.id)
+    assert.equal(body.groupRequest.groupId, group.id)
+    assert.equal(body.groupRequest.status, 'PENDING')
+  })
+
+  group.before(async () => {
+    const plainPassword = 'test'
+    const newUser = await UserFactory.merge({ password: plainPassword }).create()
+
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email: newUser.email, password: plainPassword })
+      .expect(201)
+
+    token = body.token.token
+    user = newUser
+  })
+
+  group.after(async () => {
+    await supertest(BASE_URL).delete('/sessions').set('Authorization', `Bearer ${token}`)
+  })
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+})
