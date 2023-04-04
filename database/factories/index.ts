30ae07128272bf06a41031dce3ff254579cf1c40
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-04 13:02:16 +0100

    feat(GroupRequest): creation GroupRequestFactory

--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,4 +1,5 @@
 import Factory from '@ioc:Adonis/Lucid/Factory'
+import Group from 'App/Models/Group'
 import User from 'App/Models/User'
 
 export const UserFactory = Factory.define(User, ({ faker }) => {
@@ -7,5 +8,15 @@ export const UserFactory = Factory.define(User, ({ faker }) => {
     email: faker.internet.email(),
     password: faker.internet.password(),
     avatar: faker.internet.url(),
+  }
+}).build()
+
+export const GroupFactory = Factory.define(Group, ({ faker }) => {
+  return {
+    name: faker.name.findName(),
+    description: faker.lorem.paragraph(),
+    schedule: faker.date.weekday(),
+    location: faker.internet.url(),
+    chronic: faker.lorem.sentence(),
   }
 }).build()
