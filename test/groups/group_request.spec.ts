0906711a1c732e7ad71fe2b1852a704bcdd73a99
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-07 00:48:40 +0100

    test(GroupRequest): Should accepted a group request

--- test/groups/group_request.spec.ts
+++ test/groups/group_request.spec.ts
@@ -131,6 +131,31 @@ test.group('Group Request', (group) => {
     assert.equal(body.status, 422)
   })
 
+  test.only('Should accepted a group request', async (assert) => {
+    const master = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    const { body } = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({})
+
+    const response = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests/${body.groupRequest.id}/accept`)
+      .expect(200)
+
+    assert.exists(response.body.groupRequest, 'GroupRequest undefined')
+    assert.equal(response.body.groupRequest.userId, user.id)
+    assert.equal(response.body.groupRequest.groupId, group.id)
+    assert.equal(response.body.groupRequest.status, 'ACCEPTED')
+
+    await group.load('players')
+
+    assert.isNotEmpty(group.players)
+    assert.equal(group.players.length, 1)
+    assert.equal(group.players[0].id, user.id)
+  })
+
   group.before(async () => {
     const plainPassword = 'test'
     const newUser = await UserFactory.merge({ password: plainPassword }).create()
