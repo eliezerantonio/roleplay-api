080f1f3313996be98faf85032e1346c13358d449
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-07 21:47:20 +0100

    feat(Bouncer): define new authorization

--- start/bouncer.ts
+++ start/bouncer.ts
@@ -6,6 +6,7 @@
  */
 
 import Bouncer from '@ioc:Adonis/Addons/Bouncer'
+import GroupRequest from 'App/Models/GroupRequest'
 import User from 'App/Models/User'
 
 /*
@@ -33,6 +34,12 @@ import User from 'App/Models/User'
 export const { actions } = Bouncer.define('updateUser', (user: User, updatedUser: User) => {
   return user.id === updatedUser.id
 })
+  .define('acceptGroupRequest', (user: User, groupRequest: GroupRequest) => {
+    return user.id === groupRequest.group.master
+  })
+  .define('rejectGroupRequest', (user: User, groupRequest: GroupRequest) => {
+    return user.id === groupRequest.group.master
+  })
 
 /*
 |--------------------------------------------------------------------------
