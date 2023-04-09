56bd65e3ba9c3b85d2752d5de4e5c4205548af67
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-09 18:29:01 +0100

    test(Group): should return all groups when no query is provided to list groups

--- test/groups/group.spec.ts
+++ test/groups/group.spec.ts
@@ -192,6 +192,44 @@ test.group('Group', (group) => {
     assert.equal(body.status, 404)
   })
 
+  test.only('it should return all groups when no query is provided to list groups', async (assert) => {
+    const groupPayload = {
+      name: 'test',
+      description: 'test',
+      schedule: 'test',
+      location: 'test',
+      chronic: 'test',
+      master: user.id,
+    }
+    const response = await supertest(BASE_URL)
+      .post('/groups')
+      .set('Authorization', `Bearer ${token}`)
+      .send(groupPayload)
+      .expect(201)
+
+    const group = response.body.group
+
+    const { body } = await supertest(BASE_URL)
+      .get('/groups')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+
+    assert.exists(body.groups, 'Groups undefined')
+    assert.equal(body.groups.length, 1)
+    assert.equal(body.groups[0].id, group.id)
+    assert.equal(body.groups[0].name, group.name)
+    assert.equal(body.groups[0].location, group.location)
+    assert.equal(body.groups[0].description, group.description)
+    assert.equal(body.groups[0].schedule, group.schedule)
+    assert.exists(body.groups[0].masterUser, 'Master undefined')
+    assert.equal(body.groups[0].masterUser.id, user.id)
+    assert.equal(body.groups[0].masterUser.username, user.username)
+
+    assert.isNotEmpty(body.groups[0].players, 'Empty Players')
+    assert.equal(body.groups[0].players[0].id, user.id)
+    assert.equal(body.groups[0].players[0].email, user.email)
+    assert.equal(body.groups[0].players[0].username, user.username)
+  })
   group.before(async () => {
     const plainPassword = 'test'
     const newUser = await UserFactory.merge({ password: plainPassword }).create()
