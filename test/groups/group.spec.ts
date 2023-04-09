0015a68152720c9ee277f523ef2359c07f4838e9
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-09 18:55:28 +0100

    test: should return  groups by user id

--- test/groups/group.spec.ts
+++ test/groups/group.spec.ts
@@ -231,7 +231,7 @@ test.group('Group', (group) => {
     assert.equal(body.groups[0].players[0].username, user.username)
   })
 
-  test.only('it should return no groups by user id', async (assert) => {
+  test('it should return no groups by user id', async (assert) => {
     const groupPayload = {
       name: 'test',
       description: 'test',
@@ -242,6 +242,45 @@ test.group('Group', (group) => {
     }
     const response = await supertest(BASE_URL)
       .post('/groups?user=123')
+      .set('Authorization', `Bearer ${token}`)
+      .send(groupPayload)
+      .expect(201)
+
+    const group = response.body.group
+
+    const { body } = await supertest(BASE_URL)
+      .get('/groups')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+
+    assert.exists(body.groups, 'Groups undefined')
+    assert.equal(body.groups.length, 1)
+    assert.equal(body.groups[0].id, group.id)
+    assert.equal(body.groups[0].name, group.name)
+    assert.equal(body.groups[0].location, group.location)
+    assert.equal(body.groups[0].description, group.description)
+    assert.equal(body.groups[0].schedule, group.schedule)
+    assert.exists(body.groups[0].masterUser, 'Master undefined')
+    assert.equal(body.groups[0].masterUser.id, user.id)
+    assert.equal(body.groups[0].masterUser.username, user.username)
+
+    assert.isNotEmpty(body.groups[0].players, 'Empty Players')
+    assert.equal(body.groups[0].players[0].id, user.id)
+    assert.equal(body.groups[0].players[0].email, user.email)
+    assert.equal(body.groups[0].players[0].username, user.username)
+  })
+
+  test('it should return  groups by user id', async (assert) => {
+    const groupPayload = {
+      name: 'test',
+      description: 'test',
+      schedule: 'test',
+      location: 'test',
+      chronic: 'test',
+      master: user.id,
+    }
+    const response = await supertest(BASE_URL)
+      .post(`/groups?user=${user.id}`)
       .set('Authorization', `Bearer ${token}`)
       .send(groupPayload)
       .expect(201)
