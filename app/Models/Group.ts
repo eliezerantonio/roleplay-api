9f34c2dac5c4ad24e9e233ff9e5e7953c4d9720e
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-04-10 01:08:19 +0100

    feat(Group): using scope to filter

--- app/Models/Group.ts
+++ app/Models/Group.ts
@@ -3,12 +3,15 @@ import {
   BaseModel,
   BelongsTo,
   ManyToMany,
+  ModelQueryBuilderContract,
   belongsTo,
   column,
   manyToMany,
+  scope,
 } from '@ioc:Adonis/Lucid/Orm'
 import User from './User'
 
+type Builder = ModelQueryBuilderContract<typeof Group>
 export default class Group extends BaseModel {
   @column({ isPrimary: true })
   public id: number
@@ -46,4 +49,14 @@ export default class Group extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  public static withPlayer = scope((query: Builder, userId: number) => {
+    query.whereHas('players', (query) => {
+      query.where('id', userId)
+    })
+  })
+
+  public static withText = scope((query: Builder, text: string) => {
+    query.where('name', 'LIKE', `%${text}%`).orWhere('description', 'LIKE', `%${text}%`)
+  })
 }
